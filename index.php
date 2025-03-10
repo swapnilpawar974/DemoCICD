517dd7892de91e9e72faddae50f442e993e89281
Swapnil Pawar <pawarswapnil974@gmail.com>
2025-03-10 07:47:36 +0000

    reverted the changes

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 
 <header class="header">
 
-    <a href="#" class="logo"> <i class="fa-solid fa-camera"></i> Testing Of CICD pipepline</a>
+    <a href="#" class="logo"> <i class="fa-solid fa-camera"></i> Saheli Digital Studios</a>
 
     <nav class="navbar">
         <a href="#home">home</a>
