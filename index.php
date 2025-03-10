96915d349b5e3a9005f2c8c5d49c22efbfb4bb09
Swapnil Pawar <pawarswapnil974@gmail.com>
2025-03-10 07:42:11 +0000

    Changed the heading

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 
 <header class="header">
 
-    <a href="#" class="logo"> <i class="fa-solid fa-camera"></i> Saheli Digital Studios</a>
+    <a href="#" class="logo"> <i class="fa-solid fa-camera"></i> Testing Of CICD pipepline</a>
 
     <nav class="navbar">
         <a href="#home">home</a>
